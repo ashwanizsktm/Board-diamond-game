a85d9fa3dfe185258fd44aef9903438c13e9213d
Ashwani <ashwani.kumar@artivatic.ai>
2020-06-25 17:08:09 -0700

    all squre-box filled with question-mark and randomly generated diamond-icon

--- script/app.js
+++ script/app.js
@@ -1,8 +1,7 @@
 // Getting section globally that can be used throughout the app.
 let gameBoard = document.querySelector('.gameboard');
 
-
-for(let i = 0; i < 64; i++){
+for (let i = 0; i < 64; i++) {
   let squareBox = document.createElement('div');
   let quesIcon = document.createElement('img');
   quesIcon.src = '../Assets/question-mark.png';
@@ -13,7 +12,10 @@ for(let i = 0; i < 64; i++){
   gameBoard.appendChild(squareBox);
 }
 
-function getDiamondImg(){
-  e.style.display = 'none';
-}
-
+let n = 8;
+var arr = Array.from(gameBoard.children);
+let shuffled = arr.sort(() => 0.5 - Math.random());
+let selected = shuffled.slice(0, n);
+selected.forEach((item) => {
+  item.innerHTML = `<img src="../Assets/diamond.jpg" class="diamond-icon"/>`;
+});
