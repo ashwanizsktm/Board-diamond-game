bc97326e450b82cd89ef00efbbe56094ffb34d72
Ashwani <ashwani.kumar@artivatic.ai>
2020-06-28 12:39:47 -0700

    hints functionalities added while searching the diammond

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Board-diamond-game
+
+# 1. Clone the Repo
+
+# 2 This Repo will contain mainly 3 folders and 1 index.html file
+ 
+   # The Folders are as follows 
+   # (1) Assets :- where all the essential icons are placed.
+   # (2) Styles:- where style.css are kept,
+   # (3) Script:- where app.js file is available in which all the logics are written for this Game App.
+
+# 3. After Cloning the App open in any editor then open with the live server. because few time assets doesn't 
+# get loaded generally. 
--- script/app.js
+++ script/app.js
@@ -3,8 +3,19 @@ let gameBoardWrap = document.querySelector('.gameboard-wrap')
 let gameBoard = document.querySelector('.gameboard');
 let rolledBox = document.querySelector('.number');
 let userScorePara = document.querySelector('.user-score');
-let gameOver = document.createElement('h1'); 
- 
+let gameOver = document.createElement('h1');
+
+// create Arrow icon globally.
+// left arrow
+let leftArrowImg = document.createElement('img');
+leftArrowImg.src = '../Assets/left-arrow.png';
+leftArrowImg.setAttribute('class', 'arrow-img common-icon');
+
+// right Arrow
+let rightArrowImg = document.createElement('img');
+rightArrowImg.src = '../Assets/right-arrow.png';
+rightArrowImg.setAttribute('class', 'arrow-img common-icon');
+
 // Globalvariable
 let diamondCnt = 0;
 let userScore = 0;
@@ -15,13 +26,7 @@ let totalBoxNum = 64;
 gameOver.className = 'game-over';
 gameBoardWrap.appendChild(gameOver);
 rolledBox.textContent = `Number of boxes turned:${count}`;
-userScorePara.textContent = `User Score:${userScore}`;
-
-
-// create Arrow icon globally.
-let arrowImg = document.createElement('img');
-arrowImg.src = '../Assets/right-arrow.png';
-arrowImg.setAttribute('class', 'arrow-img common-icon');
+userScorePara.textContent = `User Initial Score:${userScore}`;
 
 // Generating 64 boxes with with question mark initially dymaically.
 for (let i = 0; i < 64; i++) {
@@ -41,6 +46,7 @@ var arr = Array.from(gameBoard.children);
 let shuffled = arr.sort(() => 0.5 - Math.random());
 let selected = shuffled.slice(0, n);
 console.log(selected);
+
 selected.forEach((item) => {
   item.innerHTML = `
   <img src="../Assets/diamond.jpg" class="diamond-icon common-icon"/>
@@ -50,12 +56,40 @@ selected.forEach((item) => {
 
 // This function get triggered while clicking the box.
 function getDiamondImg(parent, children) {
+
+  // Getting Previous and next sibling onclick of box
+  let next = parent.nextElementSibling;
+  let previous = parent.previousElementSibling;
+  if (previous == null) {
+    previous = document.createElement("div");
+  } else if (next == null) {
+    next = document.createElement("div");
+  }
+
   let squareBoxChild = [...children];
   squareBoxChild.forEach((child) => {
+
+    /* Start of Advanced Section where user get hints as arrow while searching diamond*/
+    if (
+      next.children.length != 0 &&
+      children.length != 2 &&
+      next.children[0].classList.contains("diamond-icon")
+    ) {
+      parent.appendChild(rightArrowImg);
+    } else if (
+      previous.children.length != 0 &&
+      children.length != 2 &&
+      previous.children[0].classList.contains("diamond-icon")
+    ) {
+      parent.appendChild(leftArrowImg);
+    }
+    /* End of Advanced Section code*/
+
     if (child.classList.contains("ques-icon")) {
       parent.removeChild(child);
       count++;
     }
+
     if (child.classList.contains("diamond-icon")) {
       diamondCnt++;
     }
@@ -63,13 +97,14 @@ function getDiamondImg(parent, children) {
     if (diamondCnt == 8) {
       gameBoard.style.pointerEvents = "none";
       userScore = totalBoxNum - count;
-      userScorePara.innerHTML = `User Score: ${userScore}`;
+      userScorePara.innerHTML = `User Final Score: ${userScore}`;
 
       setTimeout(() => {
         gameOver.textContent = `game over`;
       }, 500);
     }
   });
+
   // Track of no. of trurned boxex.
   rolledBox.innerHTML = `Number of boxes turned:${count}`;
 }
